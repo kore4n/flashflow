1a0947f9dab301434048e41d2ee50db2e1a906d3
jkwan86 <jkwan86@uwo.ca>
2024-04-06 16:57:45 -0400

    Changed textboxes style

--- src/renderer/src/components/cards/add_card_window/CardTagging.tsx
+++ src/renderer/src/components/cards/add_card_window/CardTagging.tsx
@@ -91,7 +91,7 @@ const CardTagging: React.FC<TagsInputProps> = ({ tempTagPool, setTags }) => {
   }
 
   return (
-    <div>
+    <div className=" mb-6">
       {tempTagPool.map((tag, index) => (
         <div
           key={index}
@@ -147,6 +147,7 @@ const CardTagging: React.FC<TagsInputProps> = ({ tempTagPool, setTags }) => {
           }}
         ></span>
         <input
+          className="min-h-4 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
           ref={inputRef}
           value={inputValue}
           onChange={handleInputChange}
