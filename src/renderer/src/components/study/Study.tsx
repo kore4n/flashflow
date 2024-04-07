e5700f501d5cd3b30b4b48c21f462332733e240e
egrant47 <egrant47@uwo.ca>
2024-04-07 16:25:58 -0400

    Study Session additions

--- src/renderer/src/components/study/Study.tsx
+++ src/renderer/src/components/study/Study.tsx
@@ -1,5 +1,6 @@
 import { ActiveTab, Deck } from 'src/types/types';
 import { useEffect, useState } from 'react';
+import  MagnifyingGlassIcon from './MagnifyingGlassIcon'
 
 
 /*
@@ -9,6 +10,12 @@ function Study({ activeTab }: { activeTab: ActiveTab }): JSX.Element {
 	const [decks, setDecks] = useState<Deck[]>([]);
 	const [selectedDecks, setSelectedDecks] = useState<Deck[]>([]);
 	const [showMessage, setShowMessage] = useState<boolean>(false);
+	const [inspectDeck, setInspectDeck] = useState<Deck>();
+	const [deckInfo, setDeckInfo] = useState<{ new: number; learning: number; toReview: number }>({
+        new: 0,
+        learning: 0,
+        toReview: 0,
+    });
 
     const handleStartStudy = () => {
         if (selectedDecks.length === 0) {
@@ -16,6 +23,33 @@ function Study({ activeTab }: { activeTab: ActiveTab }): JSX.Element {
         } else {
             setShowMessage(false);
             window.api.openStudySessionWindow();
+        }
+    };
+
+	const handleInspectDeck = (deck) => {
+		setInspectDeck(deck);
+		countCardStatus(deck);
+	  };
+
+
+	  const countCardStatus = (deck) => {
+        if (deck) {
+            const cardStatusCounts = {
+                1: 0,
+                2: 0,
+                3: 0,
+                4: 0,
+            };
+
+            deck.cards.forEach((card) => {
+                cardStatusCounts[card.cardStatus]++;
+            });
+
+            setDeckInfo({
+                new: cardStatusCounts[1],
+                learning: cardStatusCounts[4],
+                toReview: cardStatusCounts[2] + cardStatusCounts[3],
+            });
         }
     };
 
@@ -40,36 +74,59 @@ function Study({ activeTab }: { activeTab: ActiveTab }): JSX.Element {
 	if (activeTab !== 'Study') return <></>;
 
 	return (
-        <div id="Study" className="mt-8 flex flex-col h-screen">
-            <div className="flex-grow">
-                <h1 className="text-xl font-bold text-white mb-4">Choose Decks to Study</h1>
-                <div className="deck-list grid gap-2 border rounded p-4">
-                    {decks.map((deck) => (
-                        <label key={deck.name} className="flex items-center gap-2 text-white">
-                            <input
-                                type="checkbox"
-                                checked={selectedDecks.some((selectedDeck) => selectedDeck.name === deck.name)}
-                                onChange={() => handleSelectDeck(deck)}
-                                className="form-checkbox h-5 w-5 text-blue-500"
-                            />
-                            <span className="text-lg">{deck.name}</span>
-                        </label>
-                    ))}
-                </div>
-                <div className="flex justify-center items-center mt-20">
-                    {showMessage && <p className="text-red-500">You need to select at least one deck to study.</p>}
-                    <button onClick={handleStartStudy} className="bg-blue-500 hover:bg-blue-00 text-white font-bold py-2 px-4 rounded uppercase">Study Now</button>
-                </div>
-            </div>
-            <div className="flex justify-center items-center mt-20 mb-20">
-                <button className="bg-slate-800 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded uppercase text-sm">Options</button>
-                <button className="bg-slate-800 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded uppercase text-sm">Custom Study</button>
-                <button className="bg-slate-800 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded uppercase text-sm">Description</button>
-            </div>
-        </div>
-    );
-}
-
+		<div id="Study" className="mt-8 flex flex-col h-screen">
+		  <div className="flex-grow flex">
+	
+			{/* Left section with checkboxes */}
+			<div className="w-1/2 pr-6 mt-11">
+			  
+			  <div className="grid gap-1 border rounded-lg border-slate-800 bg-slate-700 shadow-lg rounded p-6">
+			  <h1 className="text-xl font-bold text-white mb-4">CHOOSE DECKS TO STUDY</h1>
+				{decks.map((deck) => (
+				  <div key={deck.name} className="flex items-center gap-2 text-white">
+					<input
+					  type="checkbox"
+					  checked={selectedDecks.some((selectedDeck) => selectedDeck.name === deck.name)}
+					  onChange={() => handleSelectDeck(deck)}
+					  className="form-checkbox h-5 w-4 border border-black"
+					/>
+					<span className="text-lg">{deck.name}</span>
+					<button onClick={() => handleInspectDeck(deck)}>
+					  <MagnifyingGlassIcon width="13px" height="13px" colour="#FFFFFF" />
+					</button>
+				  </div>
+				))}
+			  </div>
+			  {showMessage && <p className="text-red-300 mt-2">You need to select at least one deck to study.</p>}
+			</div>
+	
+			{/* Right section to display deck details */}
+			{inspectDeck && (
+			  <div className="w-1/2 pl-6 mt-5">
+				<div className="border border-slate-800 shadow-md rounded-lg p-4 mt-6 bg-slate-700">
+				  <h2 className="text-lg font-bold mb-2 text-white">DECK INFO:</h2>
+				  <p className="mt-4 text-white font-bold text-center ">{inspectDeck.name}</p>
+				  <p className="text-white">New: {deckInfo.new}</p>
+                  <p className="text-white">Learning: {deckInfo.learning}</p>
+                  <p className="text-white">To Review: {deckInfo.toReview}</p>
+				</div>
+			  </div>
+			)}
+		  </div>
+	
+		  <div className="flex justify-center items-center mt-4">
+			<button onClick={handleStartStudy} className="bg-blue-500 hover:bg-blue-00 text-white font-bold py-2 px-4 rounded uppercase">Study Now</button>
+		  </div>
+	
+		  <div className="flex justify-center items-center mt-20 mb-20">
+			<button className="bg-slate-800 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded uppercase text-sm">Options</button>
+			<button className="bg-slate-800 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded uppercase text-sm">Custom Study</button>
+			<button className="bg-slate-800 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded uppercase text-sm">Description</button>
+		  </div>
+		</div>
+	  );
+	}
+	
 
 
 
